a206feecc234c562e22055ecfe400e69c4f97c52
Xylitol311 <cktnghd0311@daum.net>
2025-02-03 06:53:31 +0900

    :test_tube: test: WebSocket 연결 테스트 추가
    - MatchResponseDto에 기본 생성자 어노테이션 추가

--- Server/BE_GalaxyTalk/Match/src/main/java/com/example/match/dto/MatchResponseDto.java
+++ Server/BE_GalaxyTalk/Match/src/main/java/com/example/match/dto/MatchResponseDto.java
@@ -2,11 +2,13 @@ package com.example.match.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.Map;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class MatchResponseDto {
     private String type;
     private String message;
