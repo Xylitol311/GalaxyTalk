b6135c2a6f5930b20a7c6d51fbd875864077a501
박유진 <aaaz425@naver.com>
2025-02-04 09:25:00 +0900

    공통 컴포넌트 구현

---
+++ Client/src/shared/config/constants.ts
@@ -0,0 +1,9 @@
+export const BREAKPOINT = {
+    MOBILE: 768,
+    TABLET: 1024,
+    DESKTOP: 1280,
+};
+
+export const DELAY = {
+    DEBOUNCE: 300,
+};
---
+++ Client/src/shared/model/hooks/useIsMobile.tsx
@@ -0,0 +1,26 @@
+import { useEffect, useState } from 'react';
+import { BREAKPOINT, DELAY } from '@/shared/config/constants';
+import { debounce } from '@/shared/lib/utils';
+
+export default function useIsMobile(
+    breakpoint: number = BREAKPOINT.MOBILE,
+    debounceDelay: number = DELAY.DEBOUNCE
+): boolean {
+    const [isMobile, setIsMobile] = useState<boolean>(
+        window.innerWidth < breakpoint
+    );
+
+    const handleResize = debounce(() => {
+        setIsMobile(window.innerWidth < breakpoint);
+    }, debounceDelay);
+
+    useEffect(() => {
+        window.addEventListener('resize', handleResize);
+
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+    }, [handleResize]);
+
+    return isMobile;
+}
