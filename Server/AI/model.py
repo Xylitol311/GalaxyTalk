af73816c7c2bc85ff3b664937b9cf6655117bf4b
chanu <chanu48@naver.com>
2025-02-10 17:12:55 +0900

    🔧 AI 서버 local/prod 환경 분리

--- Server/AI/model.py
+++ Server/AI/model.py
@@ -1,18 +1,22 @@
+# model.py
 import torch
 from transformers import AutoTokenizer, AutoModelForSequenceClassification
+from config import get_config
+
+Config = get_config()
 
 class SentenceSimilarity:
     def __init__(self):
-         self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-         self.tokenizer = AutoTokenizer.from_pretrained('snunlp/KR-ELECTRA-discriminator')
-         self.model = AutoModelForSequenceClassification.from_pretrained(
-            './sentence_similarity_model_klue',
+        self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+        self.tokenizer = AutoTokenizer.from_pretrained(Config.MODEL_NAME)
+        self.model = AutoModelForSequenceClassification.from_pretrained(
+            Config.MODEL_PATH,
             num_labels=1,
             problem_type="regression",
             torch_dtype=torch.float16
-         ).to(self.device)
+        ).to(self.device)
 
-         self.model.eval()  # Set to evaluation mode
+        self.model.eval()  # Set to evaluation mode
    
     def compute_similarity(self, sent1, sent2):
         print(f"Input sentences: '{sent1}', '{sent2}'")
@@ -28,7 +32,7 @@ class SentenceSimilarity:
         attention_mask = encoded['attention_mask'].to(self.device)
          
         with torch.amp.autocast(device_type='cuda'):
-           outputs = self.model(input_ids, attention_mask=attention_mask)
-           similarity = torch.sigmoid(outputs.logits)
+            outputs = self.model(input_ids, attention_mask=attention_mask)
+            similarity = torch.sigmoid(outputs.logits)
     
         return similarity.item()
