96d64c36e0f9c04ecade4f2d795762d458a5099b
차수홍 <soohong0311@gmail.com>
2025-02-11 18:40:54 +0900

    :recycle: refact match url 로컬/환경 변수 수정

--- Server/BE_GalaxyTalk/Match/src/main/java/com/example/match/config/WebClientConfig.java
+++ Server/BE_GalaxyTalk/Match/src/main/java/com/example/match/config/WebClientConfig.java
@@ -1,5 +1,6 @@
 package com.example.match.config;
 
+import jakarta.annotation.PostConstruct;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
@@ -14,12 +15,20 @@ import reactor.core.publisher.Mono;
 @Configuration
 @Slf4j
 public class WebClientConfig {
-    @Value("${ai.ip.url}")
-    private static String aiServiceBaseUrl;
-    @Value("${chat.service.url}")
-    private static String chatServiceBaseUrl;
-    @Value("${auth.service.url}")
-    private static String authServiceBaseUrl;
+    @Value("${ai.service.url}")
+    private String aiServiceBaseUrl;
+
+    @Value("${auth.service.name}")  // 서비스 이름 주입
+    private String authServiceName;
+
+    @Value("${chat.service.name}")  // 서비스 이름 주입
+    private String chatServiceName;
+
+    @PostConstruct
+    public void init() {
+        log.info("Initialized WebClient with chat service name: {}", chatServiceName);
+        log.info("Initialized WebClient with auth service name: {}", authServiceName);
+    }
 
     @Bean
     @LoadBalanced // Eureka 기반 로드밸런싱 적용
@@ -43,14 +52,14 @@ public class WebClientConfig {
     @Bean
     public WebClient chatServiceClient(WebClient.Builder webClientBuilder) {
         return webClientBuilder
-                .baseUrl(chatServiceBaseUrl) // Eureka 서비스명 사용
+                .baseUrl("http://" + chatServiceName)
                 .build();
     }
 
     @Bean
     public WebClient authServiceClient(WebClient.Builder webClientBuilder) {
         return webClientBuilder
-                .baseUrl(authServiceBaseUrl) // Eureka 서비스명 사용
+                .baseUrl("http://" + authServiceName)
                 .build();
     }
 
--- Server/BE_GalaxyTalk/Match/src/main/java/com/example/match/controller/MatchController.java
+++ Server/BE_GalaxyTalk/Match/src/main/java/com/example/match/controller/MatchController.java
@@ -148,7 +148,7 @@ public class MatchController {
      */
     @GetMapping("/test")
     public ResponseEntity<ApiResponseDto> test(
-            String concern1, String concern2) {
+            @RequestParam String concern1, @RequestParam String concern2) {
         log.info("Test 요청 전송됨: " + concern1);
         UserMatchStatus user1 = new UserMatchStatus();
         UserMatchStatus user2 = new UserMatchStatus();
