d9244b109a0850d56dba60a63a666403e27bef13
박도아 <sk05432@naver.com>
2025-02-11 16:39:23 +0900

    :feat:카카오 로그인 api 추가 구현

---
+++ Server/BE_GalaxyTalk/Auth/src/main/java/com/galaxytalk/auth/dto/KakaoResponse.java
@@ -0,0 +1,24 @@
+package com.galaxytalk.auth.dto;
+
+import java.util.Map;
+
+public class KakaoResponse implements OAuth2Response{
+
+    private Map<String, Object> attribute;
+
+    public KakaoResponse(Map<String, Object> attribute) {
+        this.attribute = attribute;
+
+    }
+
+    @Override
+    public String getProvider() {
+        return "kakao";
+    }
+
+    @Override
+    public String getProviderId() {
+        return attribute.get("id").toString();
+    }
+
+}
