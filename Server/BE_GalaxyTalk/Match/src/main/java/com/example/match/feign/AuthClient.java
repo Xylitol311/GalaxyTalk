a61bdf7519f106473b6b4d86eeffe2e9facb12d9
Xylitol311 <cktnghd0311@daum.net>
2025-02-12 07:28:39 +0900

    chore: 설정 파일 누락됨. 제발 되라

---
+++ Server/BE_GalaxyTalk/Match/src/main/java/com/example/match/feign/AuthClient.java
@@ -0,0 +1,19 @@
+package com.example.match.feign;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@FeignClient(name="auth-service")
+public interface AuthClient {
+
+    //api 경로 & controller에 등록된 정보 가져오기
+    @GetMapping("/api/oauth")
+    ResponseEntity<?> getUser(@RequestHeader("X-User-ID") String serialNumber);
+
+    @PostMapping("/api/oauth/status")
+    ResponseEntity<?> changeUserStatus(@RequestHeader("X-User-ID") String serialNumber, @RequestParam("userInteractionState") String userInteractionState);
+}
