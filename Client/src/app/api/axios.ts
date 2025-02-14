d554a4f705539ae95c2ada09c4d763369d7d94df
aaaz425 <aaaz425@naver.com>
2025-02-14 11:26:13 +0900

    🐛 무한 재귀 에러 수정정

--- Client/src/app/api/axios.ts
+++ Client/src/app/api/axios.ts
@@ -1,5 +1,4 @@
 import axios from 'axios';
-import { PATH } from '../config/constants';
 import { BASE_URL, VERSION } from '../config/constants/path';
 
 export const fetcher = axios.create({
@@ -23,31 +22,9 @@ fetcher.interceptors.request.use(
 // Memo: axios 응답 인터셉터 설정
 fetcher.interceptors.response.use(
     (response) => response,
-    async (error) => {
-        const originalRequest = error.config;
-
-        if (!error.response) {
-            console.error('네트워크 오류 또는 CORS 문제 발생:', error);
-            return Promise.reject(error);
-        }
-
-        if (error.response?.status === 401 && !originalRequest._retry) {
-            originalRequest._retry = true;
-            try {
-                await axios.post(
-                    `${BASE_URL}/${VERSION}${PATH.API_PATH.OAUTH.REFRESH}`,
-                    {},
-                    {
-                        withCredentials: true,
-                    }
-                );
-
-                return fetcher(originalRequest);
-            } catch (refreshError) {
-                console.error('토큰 갱신 실패:', refreshError);
-                window.location.href = '/';
-            }
-        }
+    (error) => {
+        // Todo: 에러 처리
+        console.error('API 요청 실패:', error);
         return Promise.reject(error);
     }
 );
