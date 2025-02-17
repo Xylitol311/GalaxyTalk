900d3e8679aae98bf41708b339a629f608f08848
aaaz425 <aaaz425@naver.com>
2025-02-17 10:38:50 +0900

    🐛 파싱 방법 수정

--- Client/src/pages/match/index.tsx
+++ Client/src/pages/match/index.tsx
@@ -2,7 +2,6 @@ import { ExitIcon } from '@radix-ui/react-icons';
 import { Html } from '@react-three/drei';
 import { Canvas } from '@react-three/fiber';
 import { Client } from '@stomp/stompjs';
-import { parse, stringify } from 'flatted';
 import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router';
 import SockJS from 'sockjs-client';
@@ -59,11 +58,13 @@ export default function MatchingRoom() {
                 console.log('메시지 바디 출력 :', message.body);
                 // console.log('메시지 바디 타입 출력 :', message.body.type);
                 // console.log('메시지 바디 데이터 출력 :', message.body.data);
-                const stringifiedData = stringify(message.body);
+                const stringifiedData = JSON.stringify(message.body);
                 console.log('직렬화 : ', stringifiedData);
 
-                const data = parse(stringifiedData);
+                const data = JSON.parse(stringifiedData);
                 console.log('파싱 데이터 : ', data);
+                console.log(data);
+                console.log('데이터 타입 : ', data.type);
 
                 if (data.type === 'MATCH_SUCCESS') {
                     console.log(data.data);
