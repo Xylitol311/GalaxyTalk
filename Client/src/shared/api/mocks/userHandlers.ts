3cb7e94467dc17073c978628de138e3651303458
민인애 <inaemin@gmail.com>
2025-02-18 17:08:33 +0900

    🎨 add error response

--- Client/src/shared/api/mocks/userHandlers.ts
+++ Client/src/shared/api/mocks/userHandlers.ts
@@ -3,17 +3,17 @@ import { API_PATH, BASE_URL, VERSION } from '@/app/config/constants/path';
 
 export const userHandlers = [
     http.get(`${BASE_URL}/${VERSION}${API_PATH.OAUTH.INFO}`, () => {
-        // return HttpResponse.json({
-        //     success: true,
-        //     message: '유저 정보 조회에 성공했습니다',
-        //     data: {
-        //         userId: 'TQLFszqK6szLPq_uuxyMLyQqzlobDJYBSwKPu53qRvs',
-        //         energy: 15,
-        //         mbti: 'ENFJ',
-        //         planetId: 1,
-        //         role: 'USER',
-        //     },
-        // });
+        return HttpResponse.json({
+            success: true,
+            message: '유저 정보 조회에 성공했습니다',
+            data: {
+                userId: 'TQLFszqK6szLPq_uuxyMLyQqzlobDJYBSwKPu53qRvs',
+                energy: 15,
+                mbti: 'ENFJ',
+                planetId: 1,
+                role: 'USER',
+            },
+        });
 
         return HttpResponse.json(
             {
