a1c3ee3d1319f64bda5d55f1a00d7444aa111c22
차수홍 <soohong0311@gmail.com>
2025-02-18 13:06:25 +0900

    :lipstick: import문 정리, 항목 재정렬

--- Server/BE_GalaxyTalk/Support/src/main/java/com/galaxytalk/letter/dto/Users.java
+++ Server/BE_GalaxyTalk/Support/src/main/java/com/galaxytalk/letter/dto/Users.java
@@ -17,42 +17,35 @@ import java.time.LocalDateTime;
 @Setter
 @Entity
 @EntityListeners(AuditingEntityListener.class)
-@Table(name="Users")
+@Table(name = "Users")
 public class Users {
-
-    public Users(){}
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
-    @Column(nullable = false, unique=true)
+    @Column(nullable = false, unique = true)
     private String serialNumber;
-
     @Column
     private String mbti;
-
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "planet_id")
     private Planets planets;
-
     @Column(nullable = false)
     @ColumnDefault("36")
     private int energy;
-
     @Column(nullable = false)
     @ColumnDefault("0")
     private int numberOfBlocks;
-
     @Enumerated(EnumType.STRING)
     @Column(nullable = false)
     private Role role;
-
     @Column(nullable = false)
     @CreatedDate
     private LocalDateTime createdAt;
-
     @Column
     private LocalDateTime withdrawnAt;
-    
+
+    public Users() {
+    }
+
 }
