a81d18855d1fee027c7b6ccf7c637c5b2d1fee83
민인애 <inaemin@gmail.com>
2025-02-18 17:08:07 +0900

    ✨ add tooltip

--- Client/src/widget/WalkieTalkie.tsx
+++ Client/src/widget/WalkieTalkie.tsx
@@ -1,4 +1,4 @@
-import { useGLTF } from '@react-three/drei';
+import { Html, useGLTF } from '@react-three/drei';
 import { useState } from 'react';
 import RecursiveGLTF from './RecursiveGLTF';
 
@@ -23,21 +23,35 @@ function WalkieTalkie({ onClick }: WalkieTalkieProps) {
     };
 
     return (
-        <group
-            onPointerOver={handlePointerOver}
-            onPointerOut={handlePointerOut}>
-            <RecursiveGLTF
-                object={walkietalkie.scene}
-                castShadow={true}
-                receiveShadow={true}
-                scale={0.1}
-                position={[3.8, -2.8, 3]}
-                rotation={[0, (-45 * Math.PI) / 180, 0]}
-                hover={hover}
-                onClick={onClick}
-                hoverEmissiveMultiplier={200}
-            />
-        </group>
+        <>
+            {hover && (
+                <Html
+                    position={[3.8, -0.7, 3]}
+                    center
+                    transform
+                    zIndexRange={[100, 0]}
+                    style={{ pointerEvents: 'none' }}>
+                    <div className="rounded-md px-3 py-1.5 text-xs text-primary-foreground animate-in fade-in-0 zoom-in-95 data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=closed]:zoom-out-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2 ">
+                        피드백
+                    </div>
+                </Html>
+            )}
+            <group
+                onPointerOver={handlePointerOver}
+                onPointerOut={handlePointerOut}>
+                <RecursiveGLTF
+                    object={walkietalkie.scene}
+                    castShadow={true}
+                    receiveShadow={true}
+                    scale={0.1}
+                    position={[3.8, -2.8, 3]}
+                    rotation={[0, (-45 * Math.PI) / 180, 0]}
+                    hover={hover}
+                    onClick={onClick}
+                    hoverEmissiveMultiplier={200}
+                />
+            </group>
+        </>
     );
 }
 
