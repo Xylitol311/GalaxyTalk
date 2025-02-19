7bbc9afd0f696706392ff3506705ea5497b84438
inaemin <inaemin@gmail.com>
2025-02-20 00:30:30 +0900

    🎨 add UserProvider navigate logic

--- Client/src/app/config/providers/UserProvider.tsx
+++ Client/src/app/config/providers/UserProvider.tsx
@@ -1,12 +1,15 @@
 import { ReactNode, useEffect } from 'react';
+import { useNavigate } from 'react-router';
 import { useUserStore } from '@/app/model/stores/user';
 import {
     useUserInfoQuery,
     useUserStatusQuery,
 } from '@/features/user/api/queries';
+import { PATH } from '../constants';
 
 export default function UserProvider({ children }: { children: ReactNode }) {
     const { setUserBase, setUserStatus, userId } = useUserStore();
+    const navigate = useNavigate();
 
     const shouldFetch = !userId;
 
@@ -22,6 +25,24 @@ export default function UserProvider({ children }: { children: ReactNode }) {
 
         if (isStatusSuccess) {
             setUserStatus(userStatus.data);
+            if (userStatus.data.UserInteractionState !== 'chatting') {
+                // userStatus가 chatting 상태가 아니라면, 로컬스토리지의 chatdata 삭제
+                localStorage.removeItem('chatdata');
+            }
+
+            // UserInteractionState에 따른 네비게이션 처리
+            switch (userStatus.data.UserInteractionState) {
+                case 'matching':
+                    navigate(PATH.ROUTE.MATCH);
+                    break;
+                case 'chatting':
+                    navigate(PATH.ROUTE.CHAT);
+                    break;
+                case 'idle':
+                default:
+                    // idle 상태 또는 예상치 못한 상태는 현재 페이지에 머뭅니다.
+                    break;
+            }
         }
     }, [
         setUserBase,
@@ -30,6 +51,7 @@ export default function UserProvider({ children }: { children: ReactNode }) {
         userStatus,
         isInfoSuccess,
         isStatusSuccess,
+        navigate,
     ]);
 
     return <>{children}</>;
