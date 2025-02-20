81f3c95621f934352bbaa2d7d5ce8292cb9aacda
aaaz425 <aaaz425@naver.com>
2025-02-20 19:29:49 +0900

    🎨 행성 클릭 이벤트 추가

--- Client/src/widget/Planet.tsx
+++ Client/src/widget/Planet.tsx
@@ -5,6 +5,7 @@ import * as THREE from 'three';
 import { IMAGE_PATH } from '@/app/config/constants/path';
 import { WaitingUserType } from '@/features/match/model/types';
 import { formatTimeDifference } from '@/shared/lib/utils';
+import useIsMobile from '@/shared/model/hooks/useIsMobile';
 import { Card, CardContent } from '@/shared/ui/shadcn/card';
 import RecursiveGLTF from './RecursiveGLTF';
 
@@ -14,37 +15,27 @@ type PlanetProps = {
 
 export default function Planet({ userInfo }: PlanetProps) {
     const star = useGLTF(`${IMAGE_PATH}star.glb`);
-
+    const isMobile = useIsMobile();
     const meshRef = useRef<THREE.Mesh>(null);
     const [hovered, setHovered] = useState(false);
+    const [clicked, setClicked] = useState(false); // 클릭 여부 상태 추가
 
-    // 랜덤한 초기 위치 생성 (x, y, z 모두 -1.5 ~ 1.5 사이)
     const [randomPosition] = useState<[number, number, number]>(() => [
-        Math.random() * 3 - 1.5, // -1.5 ~ 1.5
-        Math.random() * 3 - 1.5, // -1.5 ~ 1.5
-        Math.random() * 3 - 1.5, // -1.5 ~ 1.5
+        Math.random() * 3 - 1.5,
+        Math.random() * 3 - 1.5,
+        Math.random() * 3 - 1.5,
     ]);
 
-    // 랜덤한 회전 속도 생성
-    const rotationSpeed = Math.random() * 0.005 + 0.005; // 0.005 ~ 0.015 사이
-
-    // 랜덤한 움직임 속도 생성
+    const rotationSpeed = Math.random() * 0.005 + 0.005;
     const oscillationSpeed = Math.random() * 0.005 + 0.002;
 
-    // 주기적으로 이동하는 위치 값 생성
     const oscillation = (time: number, speed: number) => {
-        return Math.sin(time * speed) * 0.2; // 0.2의 범위로 왔다 갔다
+        return Math.sin(time * speed) * 0.2;
     };
 
-    // 행성을 회전시키고, 위치를 주기적으로 움직이기 위한 애니메이션
     useFrame(({ clock }) => {
-        if (!meshRef.current) {
-            return;
-        }
-
-        // 호버 상태가 아닐 때만 애니메이션 실행
-        if (!hovered) {
-            // 원래 애니메이션 좌표 계산
+        if (!meshRef.current) return;
+        if (!hovered && !clicked) {
             const targetX =
                 randomPosition[0] +
                 oscillation(clock.elapsedTime, oscillationSpeed);
@@ -55,26 +46,34 @@ export default function Planet({ userInfo }: PlanetProps) {
                 randomPosition[2] +
                 oscillation(clock.elapsedTime, oscillationSpeed);
 
-            // `lerp`를 사용하여 부드럽게 보간
             meshRef.current.position.lerp(
                 new THREE.Vector3(targetX, targetY, targetZ),
                 0.1
             );
 
-            // 회전 애니메이션
             meshRef.current.rotation.y += rotationSpeed;
             meshRef.current.rotation.x += rotationSpeed;
         }
     });
 
     const handlePointerOver = () => {
-        setHovered(true);
-        document.body.style.cursor = 'pointer';
+        if (!isMobile) {
+            setHovered(true);
+            document.body.style.cursor = 'pointer';
+        }
     };
 
     const handlePointerOut = () => {
-        setHovered(false);
-        document.body.style.cursor = 'auto';
+        if (!isMobile) {
+            setHovered(false);
+            document.body.style.cursor = 'auto';
+        }
+    };
+
+    const handlePointerDown = () => {
+        if (isMobile) {
+            setClicked(true);
+        }
     };
 
     return (
@@ -82,20 +81,24 @@ export default function Planet({ userInfo }: PlanetProps) {
             <mesh
                 onPointerOver={handlePointerOver}
                 onPointerOut={handlePointerOut}
-                position={randomPosition} // 랜덤한 위치 적용
+                position={randomPosition}
                 ref={meshRef}>
-                <RecursiveGLTF object={star.scene} scale={1} />
+                <RecursiveGLTF
+                    object={star.scene}
+                    scale={1}
+                    onClick={handlePointerDown}
+                />
             </mesh>
 
-            {hovered && (
+            {(hovered || clicked) && (
                 <Html
                     position={[0, 0, 0]}
                     center
                     zIndexRange={[100, 0]}
                     style={{
                         pointerEvents: 'none',
-                        width: '360px', // 크기를 고정값으로 설정
-                        height: 'auto', // 필요에 따라 높이도 설정
+                        width: '360px',
+                        height: 'auto',
                     }}>
                     <Card className="bg-white p-6 rounded-xl w-full transform transition-all duration-300 hover:scale-105 hover:shadow-inner">
                         <CardContent>
@@ -114,6 +117,19 @@ export default function Planet({ userInfo }: PlanetProps) {
                             </div>
                         </CardContent>
                     </Card>
+                    {/* 클릭 시 다른 곳을 누르면 닫히도록 처리 */}
+                </Html>
+            )}
+            {clicked && (
+                <Html
+                    position={[0, 0, 0]}
+                    center
+                    zIndexRange={[100, 0]}
+                    className="w-screen h-screen">
+                    <div
+                        className="fixed top-0 left-0 w-full h-full z-100"
+                        onClick={() => setClicked(false)}
+                    />
                 </Html>
             )}
         </>
