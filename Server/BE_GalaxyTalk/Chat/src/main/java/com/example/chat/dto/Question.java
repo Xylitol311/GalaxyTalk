e0f4c94f32d552792d2d5836a5774646643b402c
chanu <chanu48@naver.com>
2025-02-20 18:05:27 +0900

    :wrench: 질문 생성 시 잘못됐을 경우 추가

--- Server/BE_GalaxyTalk/Chat/src/main/java/com/example/chat/dto/Question.java
+++ Server/BE_GalaxyTalk/Chat/src/main/java/com/example/chat/dto/Question.java
@@ -1,5 +1,6 @@
 package com.example.chat.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
--- Server/BE_GalaxyTalk/Chat/src/main/java/com/example/chat/service/AsyncChatService.java
+++ Server/BE_GalaxyTalk/Chat/src/main/java/com/example/chat/service/AsyncChatService.java
@@ -1,17 +1,23 @@
 package com.example.chat.service;
 
+import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.example.chat.dto.Question;
 import com.example.chat.repository.ChatRepository;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.concurrent.CompletableFuture;
 
 @RequiredArgsConstructor
@@ -51,7 +57,17 @@ public class AsyncChatService {
                     .asText();
 
             log.info("chatgpt 생성 질문: {}", content);
-            List<Question> questions = objectMapper.readValue(content, new TypeReference<List<Question>>() {});
+            List<Question> questions = new ArrayList<>();
+
+            try {
+                questions = objectMapper.readValue(content, new TypeReference<List<Question>>() {});
+            } catch (JsonParseException e) {
+                log.info("질문이 제대로 생성되지 않음.");
+                Question question = new Question();
+                question.setQuestionId("1");
+                question.setContent("유사도가 낮아 질문을 생성할 수 없습니다.");
+                questions.add(question);
+            }
 
             // 질문을 mongodb에 저장합니다.
             chatRepository.updateQuestions(chatRoomId, questions);
